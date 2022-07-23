ec15e6918d853d49b14d9f269bbf9c8ec592f2ef
Marcelo Oikawa <marcelo.oikawa@klarna.com>
2022-07-23 19:55:47 +0200

    removing simpledbf library

--- dbf2csv.py
+++ dbf2csv.py
@@ -1,8 +1,21 @@
 import os
-from simpledbf import Dbf5
+from csv import DictWriter
+from dbfread import DBF
 
 for file in os.listdir("data"):
-        if file.endswith(".dbf"):
-                dbf = Dbf5("data/" + file, codec='utf-8')
-                dbf.to_csv("data/csv/" + os.path.splitext(file)[0] + ".csv")
-                print("data/" + file + " converted to csv")
+    if file.endswith(".dbf") and ("*" not in file):
+        dbf = DBF(f"data/{file}")
+        count = 0
+        try:
+            with open(f"data/csv/{os.path.splitext(file)[0]}.csv", 'w') as csvfile:
+                for record in dbf:
+                    if count == 0:
+                        writer = DictWriter(csvfile, fieldnames=record.keys())
+                        writer.writeheader()
+                    writer.writerow(record)
+                    count = count + 1
+
+        except UnicodeDecodeError:
+            print(f"data/{file} - [unicodeDecodeError] error to convert row {count}")
+        count = 0
+        print(f"data/{file} converted to csv")
