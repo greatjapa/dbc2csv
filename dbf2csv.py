fbf390166da592b911a6da3a83e0445b68d2ffc0
leandroladislau <leandroladislau@gmail.com>
2024-11-19 21:29:56 -0300

    change to dbfread

--- dbf2csv.py
+++ dbf2csv.py
@@ -1,8 +1,10 @@
 import os
-from simpledbf import Dbf5
+import pandas as pd
+from dbfread import DBF
 
 for file in os.listdir("data"):
-        if file.endswith(".dbf") and ("*" not in file):
-                dbf = Dbf5(f"data/{file}", codec='ISO-8859-1')
-                dbf.to_csv(f"data/csv/{os.path.splitext(file)[0]}.csv")
-                print(f"data/{file} converted to csv")
+    if file.endswith(".dbf") and ("*" not in file):
+        dbf = DBF(f"data/{file}", load=True, encoding='ISO-8859-1')
+        dbf_df = pd.DataFrame(dbf.records)
+        dbf_df.to_csv(f"data/csv/{os.path.splitext(file)[0]}.csv")
+        print(f"data/{file} converted to csv")
